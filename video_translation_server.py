9fe5b67fe92ce36005d4585cd4b451a46154ade8
Mathew Atisa <matisa360@gmail.com>
2024-11-10 14:06:37 -0800

    Added lifespan with context manager to start simulation before user requet arrives

--- video_translation_server.py
+++ video_translation_server.py
@@ -1,31 +1,40 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
 import asyncio
 import random
 
+
 class VideoTranslationServer:
-    def __init__(self, min_delay: int = 0, max_delay: int = 0, error_probability: float = .1) -> None:
-        self.app = FastAPI()
+    def __init__(
+        self, min_delay: int = 0, max_delay: int = 0, error_probability: float = 0.1
+    ) -> None:
+        self.app = FastAPI(lifespan=self.lifespan)
         self.video_status = "pending"
         self.delay_amount = random.uniform(min_delay, max_delay)
         self.error_probability = error_probability
+
         self._initialize_routes()
 
-    def _initialize_routes(self) -> None:
+    def _initialize_routes(self):
         @self.app.get("/status")
-        def status():
+        async def status():
             return {"result": self.video_status}
-        
+
+    @asynccontextmanager
+    async def lifespan(self, app: FastAPI):
+        await asyncio.create_task(self.simulate_translation())
+        yield
+
     async def simulate_translation(self):
         await asyncio.sleep(self.delay_amount)
         if random.random() < self.error_probability:
             self.video_status = "error"
-        else: 
+        else:
             self.video_status = "completed"
 
 
-server = VideoTranslationServer()
-app = server.app
-
 if __name__ == "__main__":
+    server = VideoTranslationServer(min_delay=7, max_delay=8)
+    app = server.app
     uvicorn.run(app, host="0.0.0.0", port=8000)
