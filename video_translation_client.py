32fadc8e9b3d5cd218794ea1a84d32151de0ab8b
Mathew Atisa <matisa360@gmail.com>
2024-11-10 17:36:02 -0800

    added polling for request when server is down

--- video_translation_client.py
+++ video_translation_client.py
@@ -1,31 +1,42 @@
 import requests
 import time
-
+from video_translation_server import VideoTranslationServer
 
 class VideoTranslationClient:
-    def __init__(self, server_url: str, polling_interval: int = 5) -> None:
+    def __init__(self, server_url: str, 
+                 polling_interval: int = 5,
+                 max_retries: int = 5) -> None:
         self.server_url = server_url
         self.polling_interval = polling_interval
+        self.max_retries = max_retries
 
     def _make_get_request(self):
-        try:
-            response = requests.get(f"{self.server_url}/status")
-            return response.json()
-        except:
-            print("Failed to retrieve status")
+        for i in range(1, self.max_retries + 1):
+            try:
+                response = requests.get(f"{self.server_url}/status")
+                return response.json()
+            except requests.exceptions.RequestException as e:
+                wait_time = i
+                time.sleep(wait_time)
+                print(f"Failed to retrieve status trying again in {wait_time} seconds.")
+        raise RuntimeError(f"Failed to reach the server after {self.max_retries} attempts")
 
     def get_status(self):
         status_response = self._make_get_request()
         status = status_response.get("result", "error")
+        while status == "pending":
+            if status == "completed":
+                print("Video translation is completed")
+            elif status == "error":
+                print("An error occurred during the video translation process")
+            else:
+                print(
+                    "Translation is still pending, cheking again in "
+                    + str(self.polling_interval)
+                    + " seconds"
+                )
+                time.sleep(self.polling_interval)
 
-        if status == "completed":
-            print("Video translation is completed")
-        elif status == "error":
-            print("An error occurred during the video translation process")
-        else:
-            print(
-                "Translation is still pending, cheking again in "
-                + str(self.polling_interval)
-                + " seconds"
-            )
-            time.sleep(self.polling_interval)
+if __name__ == "__main__":
+    client = VideoTranslationClient(server_url="http://localhost:8000", polling_interval=5)
+    client.get_status()
--- video_translation_server.py
+++ video_translation_server.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-import uvicorn
 import asyncio
 import random
-
+import uvicorn
 
 class VideoTranslationServer:
     def __init__(
-        self,
-        error_probability: float = 0.1,
+        self, 
+        error_probability: float = 0.1, 
         length_of_translation: int = 100
     ) -> None:
         self.app = FastAPI()
@@ -26,22 +25,20 @@ class VideoTranslationServer:
 
     async def start_translation(self):
         asyncio.create_task(self.simulate_translation())
-    
+
     async def simulate_translation(self):
-        self.start_time = asyncio.get_event_loop().time()
+        self.start_time = int(asyncio.get_event_loop().time())
         while self.video_status == "pending":
-            elapsed_time = asyncio.get_event_loop().time() - self.start_time
-            if elapsed_time >= self.length_of_translation:
+            elapsed_time = int(asyncio.get_event_loop().time()) - self.start_time
+            if elapsed_time > self.length_of_translation:
                 if random.random() < self.error_probability:
                     self.video_status = "error"
                 else:
                     self.video_status = "completed"
-                print(self.video_status)
                 break
             await asyncio.sleep(1)
 
-
 if __name__ == "__main__":
-    server = VideoTranslationServer()
+    server = VideoTranslationServer(error_probability=0.1, length_of_translation=100)
     app = server.app
     uvicorn.run(app, host="0.0.0.0", port=8000)
