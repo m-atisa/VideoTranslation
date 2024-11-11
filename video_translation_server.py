46ba9a82c95bbdc2d0d529822b40b997de845f51
Mathew Atisa <matisa360@gmail.com>
2024-11-10 17:07:38 -0800

    Removed unnecessary lifespan, asynccontextmanager, implemented timed translation with asyncio

--- video_translation_server.py
+++ video_translation_server.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 import uvicorn
 import asyncio
 import random
@@ -7,34 +6,42 @@ import random
 
 class VideoTranslationServer:
     def __init__(
-        self, min_delay: int = 0, max_delay: int = 0, error_probability: float = 0.1
+        self,
+        error_probability: float = 0.1,
+        length_of_translation: int = 100
     ) -> None:
-        self.app = FastAPI(lifespan=self.lifespan)
+        self.app = FastAPI()
         self.video_status = "pending"
-        self.delay_amount = random.uniform(min_delay, max_delay)
         self.error_probability = error_probability
+        self.length_of_translation = length_of_translation
+        self.start_time = 0
 
         self._initialize_routes()
+        self.app.add_event_handler("startup", self.start_translation)
 
     def _initialize_routes(self):
         @self.app.get("/status")
         async def status():
             return {"result": self.video_status}
 
-    @asynccontextmanager
-    async def lifespan(self, app: FastAPI):
-        await asyncio.create_task(self.simulate_translation())
-        yield
-
+    async def start_translation(self):
+        asyncio.create_task(self.simulate_translation())
+    
     async def simulate_translation(self):
-        await asyncio.sleep(self.delay_amount)
-        if random.random() < self.error_probability:
-            self.video_status = "error"
-        else:
-            self.video_status = "completed"
+        self.start_time = asyncio.get_event_loop().time()
+        while self.video_status == "pending":
+            elapsed_time = asyncio.get_event_loop().time() - self.start_time
+            if elapsed_time >= self.length_of_translation:
+                if random.random() < self.error_probability:
+                    self.video_status = "error"
+                else:
+                    self.video_status = "completed"
+                print(self.video_status)
+                break
+            await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    server = VideoTranslationServer(min_delay=7, max_delay=8)
+    server = VideoTranslationServer()
     app = server.app
     uvicorn.run(app, host="0.0.0.0", port=8000)
